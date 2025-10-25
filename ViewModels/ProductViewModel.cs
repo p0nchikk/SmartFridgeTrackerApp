a41b953588af3cf2d00bdc87d2024419e7851eb3
Polinka Prokopenko <prokopenkopolinka@outlook.com>
2025-10-25 16:10:37 +0300

    Main Page 1.3 ( New feature - warning sign + ProductViewModel.cs is added )

---
+++ ViewModels/ProductViewModel.cs
@@ -0,0 +1,37 @@
+﻿using SmartFridgeTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFridgeTracker.ViewModels
+{
+    public class ProductViewModel : ViewModelBase
+    {
+        private readonly Product product;
+
+        public ProductViewModel(Product product)
+        {
+            this.product = product;
+        }
+
+        public string? Name => product.Name;
+        public int Quantity => product.Quantity;
+        public string? Image => product.Image;
+        public DateTime ExpirationDate => product.ExpirationDate;
+        public bool IsSpoiled => product.IsSpoiled;
+
+        // UI-specific property
+        public string StateIcon
+        {
+            get
+            {
+                if (IsSpoiled) return "warning_sign.png";
+                // "Soon to expire" if 2 days or less left
+                else if ((ExpirationDate - DateTime.Now).TotalDays <= 2) return "exclamation.png";
+                else return string.Empty;
+            }
+        }
+    }
+}
