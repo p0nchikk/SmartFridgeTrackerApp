4d51281cabc892531af5b1c727878a3da96b2ba7
Ponchik <prokopenkopolinka@outlook.com>
2026-04-02 21:34:12 +0300

    Creating AI service 1.0 ( Generall AI Service interface + Gemini Service )

---
+++ Services/GeminiAIService.cs
@@ -0,0 +1,44 @@
+﻿using DotNetEnv;
+using Google.GenAI;
+using Google.GenAI.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SmartFridgeTracker.Services
+{
+    public class GeminiAIService : IAIService
+    {
+        private string modelName = "gemini-3-flash";
+        private Client _client;
+        private GenerateContentConfig _config;
+        public GeminiAIService()
+        {
+            //Load the API key from the .env file
+            Env.Load(".env");
+            var apiKey = System.Environment.GetEnvironmentVariable("GEMINI_API_KEY");
+
+            //Initialize the Gemini API client
+            _client = new Client(apiKey: apiKey);
+
+            //Set configuration
+            _config = new GenerateContentConfig
+            {
+                ResponseMimeType = "application/json"
+            };
+        }
+
+        public async Task<string> GetRecipeAsync(string ingredients)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> IdentifyProductAsync(byte[] imageBytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IAIService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFridgeTracker.Services
+{
+    public interface IAIService //Contract for AI services, such as Gemini API
+    {
+        Task<string> GetRecipeAsync(string ingredients); //Get recipe based on identified ingredients
+        Task<string> IdentifyProductAsync(byte[] imageBytes); //Identify product based on image bytes (from camera capture)
+    }
+}
