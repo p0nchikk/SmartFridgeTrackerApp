bdecb76a2127ac89a37cb132af534b6d02f7d6d2
Ponchik <prokopenkopolinka@outlook.com>
2026-04-20 15:44:56 +0300

    Product Model-> new version 2.0 ( new product model has only one quantity property as a Quantity object, that consists of: current amount, total amount, quantity unit. In the future this data will be usefull in displaying progress bar near to each product + user can use 1/4, 1/2 or 3/4 of current amount )

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,7 @@ namespace SmartFridgeTracker.Models
         public string Id { get; set; } 
         public string Emoji { get; set; }
         public string? Name { get; set; }
-        public string Quantity { get; set; }
-        public int Count { get; set; } //think about        
+        public Quantity Quantity { get; set; }       
         public int LifeDays { get; set; } //in days
         public DateTime DateAdded { get; set; } = DateTime.Now; //to calculate expiration date and for sorting by date added
 
@@ -24,36 +24,22 @@ namespace SmartFridgeTracker.Models
         #endregion
 
         public Product(){ } //Parameterless constructor for Firebase deserialization
-        public Product(string id, string emoji, string name, string quantity, int count, int lifeDays)
+        public Product(string id, string emoji, string name, Quantity quantity, int count, int lifeDays)
         {
             this.Id = id;
             this.Emoji = emoji;
             this.Name = name;
             this.Quantity = quantity;
-            this.Count = count;
             this.LifeDays = lifeDays;
         }
 
-        //Constructor without id, with quantity as a float and measurement as a string, for new products that haven't been saved to Firebase yet
+        //Constructor without id, for new products that haven't been saved to Firebase yet
         public Product(string emoji, string name, float quantity, string measurement, int count, int lifeDays)
         {
             this.Id = string.Empty;
             this.Emoji = emoji;
             this.Name = name;
-            this.Quantity = $"{quantity} {measurement}";
-            this.Count = count;
-            this.LifeDays = lifeDays;
-        }
-
-
-        //Constructor without id, for new products that haven't been saved to Firebase yet
-        public Product(string emoji, string name, string quantity, int count, int lifeDays)
-        {
-            this.Id = String.Empty;
-            this.Emoji = emoji;
-            this.Name = name;
-            this.Quantity = quantity;
-            this.Count = count;
+            this.Quantity = new Quantity(quantity, count, measurement);
             this.LifeDays = lifeDays;
         }
     }
---
+++ Models/Quantity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFridgeTracker.Models
+{
+    public class Quantity
+    {
+        public float CurrentAmount { get; set; }
+        public float TotalAmount { get; set; }
+        public string QuantityUnit { get; set; }
+
+        public Quantity(float currentAmount, float totalAmount, string quantityUnit)
+        {
+            CurrentAmount = currentAmount;
+            TotalAmount = totalAmount;
+            QuantityUnit = quantityUnit;
+        }
+
+        public Quantity(float SingleAmount, int count, string quantityUnit)
+        {
+            float currentAmount = SingleAmount * count;
+            CurrentAmount = currentAmount;
+            TotalAmount = currentAmount;
+            QuantityUnit = quantityUnit;
+        }
+    }
+}
